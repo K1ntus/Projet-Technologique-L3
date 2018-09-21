aa4504a7c10cbba1583fca9890e14da9c696b136
Jordan <masson.jordan@gmail.com>
2018-09-21 17:06:55 +0200

    beginning of the windowwww

---
+++ Semestre 1/TD1/mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPlainTextEdit>
+
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QPlainTextEdit *textEdit;
+
+private slots:
+    void on_actionA_propos_triggered();
+    void on_actionQuitter_triggered();
+    void on_actionOuvrir_triggered();
+    void loadFile(const QString &fileName);
+};
+
+#endif // MAINWINDOW_H
