5e240454ef05658119db2e37823a40430197a1c2
Jordan <masson.jordan@gmail.com>
2018-09-30 16:58:18 +0200

    cleaning folder

--- Tuto/CallHello.cpp
+++
@@ -1,8 +0,0 @@
-//: C02:CallHello.cpp
-// Appeler un autre programme
-#include <cstdlib> // Déclare "system()"
-using namespace std;
-
-int main() {
-  system("./helloWorld");
-} ///:~
--- Tuto/printBinary.h
+++
@@ -1,4 +0,0 @@
-//: C03:printBinary.h
-// imprime un bit au format binaire
-void printBinary(const unsigned char val);
-///:~ 
